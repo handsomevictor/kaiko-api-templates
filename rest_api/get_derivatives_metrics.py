15d292952f97dd766cc78fe3dda486f1198efdc5
handsomevictor <857730556@qq.com>
2024-06-12 10:27:46 +0200

    Update get_derivatives_metrics.py

--- rest_api/get_derivatives_metrics.py
+++ rest_api/get_derivatives_metrics.py
@@ -25,29 +25,30 @@ def get_derivatives_metrics(url_derivatives_metrics):
             res_data = res_data + res['data']
         else:
             break
+
     try:
-        return pd.DataFrame(res_data)
+        res_data = pd.DataFrame(res_data)
+        if 'timestamp' in res_data.columns:
+            res_data['timestamp'] = pd.to_datetime(res_data['timestamp'], unit='ms')
+        return res_data
     except KeyError:
         return pd.DataFrame()
 
 
 if __name__ == '__main__':
-    exch = 'drbt'
-    start_time = '2024-06-01T00:00:00.000Z'
-    end_time = '2024-06-10T00:00:00.000Z'
-    base_assets = 'btc'
-    quote_assets = 'usd'
-    time_label = 'timestamp'
-    instrument_class = 'option'
-    instrument = "btc28jun2410000c"  # This info can be found in derivatives reference result
+    exch = 'binc'
+    start_time = '2024-06-10T00:00:00.000Z'
+    end_time = '2024-06-10T01:00:00.000Z'
+    base_assets = 'usdc'
+    quote_assets = 'usdt'
+    instrument_class = 'perpetual-future'
+    instrument = "usdc-usdt"  # This info can be found in derivatives reference result for options
 
     url_derivatives_reference = (f"https://us.market-api.kaiko.io/v2/data/derivatives.v2/reference"
                                  f"?exchange={exch}"
                                  f"&instrument_class={instrument_class}"
                                  f"&base_assets={base_assets}"
-                                 f"&quote_assets={quote_assets}"
-                                 f"&start_time={start_time}"
-                                 f"&end_time={end_time}")
+                                 f"&quote_assets={quote_assets}")
 
     url_derivatives_risk = ("https://us.market-api.kaiko.io/v2/data/derivatives.v2/risk"
                             f"?exchange={exch}"
@@ -64,9 +65,10 @@ if __name__ == '__main__':
                              f"&end_time={end_time}")
 
     df_ref = get_derivatives_metrics(url_derivatives_metrics=url_derivatives_reference)
-    df_risk = get_derivatives_metrics(url_derivatives_metrics=url_derivatives_risk)
-    df_price = get_derivatives_metrics(url_derivatives_metrics=url_derivatives_price)
-
     print(df_ref.head())
+
+    df_risk = get_derivatives_metrics(url_derivatives_metrics=url_derivatives_risk)
     print(df_risk.head())
+
+    df_price = get_derivatives_metrics(url_derivatives_metrics=url_derivatives_price)
     print(df_price.head())
